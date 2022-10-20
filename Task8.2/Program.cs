c9bf56c3d79a884ad53b2354a026dbdcd708d167
Алёна Алёна <alenchaeto1@outlook.com>
2022-10-20 22:20:02 +0300

    Добавьте файлы проекта.

---
+++ Task8.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Task\task8-2.txt";
+            Random random = new Random();
+            int sum = 0;
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int number = Convert.ToInt32(sr.ReadLine());
+                    sum += number;
+                    Console.WriteLine(number);
+                }
+            }
+            Console.WriteLine("Сумма чисел = {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task8.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Task\task8-2.txt";
+
+            int countStr = 0;
+            int countChar = 0;
+            string text = "";
+            string[] wordArray;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string stroka = sr.ReadLine();
+                    countStr++;
+                    foreach (char c in stroka)
+                    {
+                        countChar++;
+                    }
+                    text = text + stroka + " ";
+                }
+            }
+            text = text.Trim();
+            wordArray = text.Split();
+            Console.WriteLine();
+            Console.WriteLine("Строк = {0}", countStr);
+            Console.WriteLine("Слов = {0}", wordArray.Count());
+            Console.WriteLine("Символов = {0}", countChar);
+            Console.ReadKey();
+        }
+    }
+}
